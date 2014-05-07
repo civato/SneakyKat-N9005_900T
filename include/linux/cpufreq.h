77745172df607a3617c5951a951c88e21559ab21
civato <civato@ubuntu.(none)>
2014-05-07 17:15:29 +0200

    Add intellidemand governor / @ Faux123 tweaked @ dorimanx

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -423,6 +423,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_intellidemand)
 #endif
 
 
